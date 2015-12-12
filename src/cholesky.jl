a24eba365a5a4596bb07e85438e65ffbe969df58
Tim Holy <tim.holy@gmail.com>
2015-12-12 10:53:10 -0600

    Refactor API to use the blocked algorithm, and precompute X, Y
    This adds:
    - Several hemireal factorization types and algorithms for converting among representations.
    - Uses the in-place real representation for computing the factorization (cholfact!)
    - Precomputes X, Y, since those don't depend on the rhs
    
    Overall this seems like a cleaner design

--- src/cholesky.jl
+++ src/cholesky.jl
@@ -1,80 +1,92 @@
-import Base: \
+import Base: *, \, unsafe_getindex
 using Base.BLAS: syr!, ger!, syrk!, syr2k!
 using Base.LinAlg: BlasFloat
 
-immutable PureHemiCholesky{T}
+abstract AbstractHemiCholesky{T<:Real}
+
+immutable HemiCholesky{T} <: AbstractHemiCholesky{T}
     L::Matrix{PureHemi{T}}
 end
-# Pure-hemi encoded as a real (stores the nu-component in lower-triangle of L)
-immutable HemiCholeskyReal{T<:AbstractFloat}
+# Pure-hemi encoded as real (stores the nu-component in lower-triangle of L)
+immutable HemiCholeskyReal{T} <: AbstractHemiCholesky{T}
     L::Matrix{T}
     d::Vector{Int8}
 end
 
-Base.size(F::PureHemiCholesky) = size(F.L)
-Base.size(F::PureHemiCholesky, d::Integer) = size(F.L, d)
-Base.size(F::HemiCholeskyReal) = size(F.L)
-Base.size(F::HemiCholeskyReal, d::Integer) = size(F.L, d)
+immutable HemiCholeskyXY{T<:Real,Ltype<:AbstractHemiCholesky} <: AbstractHemiCholesky{T}
+    L::Ltype
+    X::Matrix{T}
+    Y::Matrix{PureHemi{T}}
+    H::Matrix{T}
+end
+HemiCholeskyXY(L::AbstractHemiCholesky, X, Y, H) = HemiCholeskyXY{eltype(X), typeof(L)}(L, X, Y, H)
 
-function Base.convert{T}(::Type{PureHemiCholesky{T}}, F::HemiCholeskyReal)
+for FT in (HemiCholesky, HemiCholeskyReal, HemiCholeskyXY)
+    @eval begin
+        Base.size(F::$FT) = size(F.L)
+        Base.size(F::$FT, d::Integer) = size(F.L, d)
+        Base.eltype{T}(::Type{$FT{T}}) = PureHemi{T}
+    end
+end
+Base.eltype{T,Ltype}(::Type{HemiCholeskyXY{T,Ltype}}) = PureHemi{T}
+
+@inline Base.getindex(F::HemiCholesky, i::Integer, j::Integer) = F.L[i,j]
+
+@inline function unsafe_getindex{T}(F::HemiCholeskyReal{T}, i::Integer, j::Integer)
+    d = F.d[j]
+    nu = F.L[i,j]
+    ifelse(d == 0 && i==j, PureHemi{T}(1,0), PureHemi{T}(d*nu, nu))
+end
+@inline function Base.getindex{T}(F::HemiCholeskyReal{T}, i::Integer, j::Integer)
+    ifelse(i >= j, Base.unsafe_getindex(F, i, j), PureHemi{T}(0, 0))
+end
+
+Base.convert{T}(::Type{HemiCholesky}, F::AbstractHemiCholesky{T}) = convert(HemiCholesky{T}, F)
+function Base.convert{T}(::Type{HemiCholesky{T}}, F::HemiCholeskyReal)
     L = Array(PureHemi{T}, size(F))
     K = size(F, 1)
     for j = 1:K
-        Ljj, d = F.L[j,j], F.d[j]
         for i = 1:j-1
             L[i,j] = zero(PureHemi{T})
         end
-        L[j,j] = d == 0 ? PureHemi{T}(1,0) : PureHemi{T}(Ljj*d, Ljj)
-        for i = j+1:K
-            Lij = F.L[i,j]
-            L[i,j] = PureHemi{T}(d*Lij, Lij)
+        for i = j:K
+            L[i,j] = F[i,j]
         end
     end
-    PureHemiCholesky(L)
+    HemiCholesky(L)
 end
-Base.convert{T}(::Type{PureHemiCholesky}, F::HemiCholeskyReal{T}) = convert(PureHemiCholesky{T}, F)
+Base.convert{T}(::Type{HemiCholesky{T}}, F::HemiCholeskyXY) = convert(HemiCholesky{T}, F.L)
 
-# Base.show(io::IO, F::HemiCholeskyReal) = show(io, convert(PureHemiCholesky, F))
+Base.convert{T}(::Type{Matrix}, F::AbstractHemiCholesky{T}) = convert(HemiCholesky{T}, F).L
+Base.convert{T}(::Type{Matrix{T}}, F::AbstractHemiCholesky) = convert(HemiCholesky{T}, F).L
 
-# Toy implementation (very cache-unfriendly)
-function Base.cholfact{T}(::Type{PureHemi{T}}, A::AbstractMatrix, δ=defaultδ(A))
-    issym(A) || error("A must be symmetric")
-    eltype(A)<:Real || error("element type $(eltype(A)) not yet supported")
-    L = fill(zero(PureHemi{T}), size(A))
-    K = size(A, 1)
-    for j = 1:K
-        s = A[j,j]
-        for k = 1:j-1
-            l = L[j,k]
-            s -= l*l
-        end
-        if abs(s) > δ
-            f = sqrt(abs(s)/2)
-            Ljj = PureHemi{T}(sign(s)*f, f)
-            L[j,j] = Ljj
-            for i = j+1:K
-                s = A[i,j]
-                for k = 1:j-1
-                    s -= L[i,k]*L[j,k]
-                end
-                L[i,j] = s/Ljj
-            end
-        else
-            L[j,j] = PureHemi{T}(1, 0)
-            for i = j+1:K
-                s = A[i,j]
-                for k = 1:j-1
-                    s -= L[i,k]*L[j,k]
-                end
-                L[i,j] = PureHemi{T}(0, s)
-            end
-        end
+function Base.show(io::IO, F::AbstractHemiCholesky)
+    println(io, Base.dims2string(size(F)), " ", typeof(F), ':')
+    _show(io, convert(Matrix, F))
+end
+
+_show(io::IO, M::Matrix) = Base.with_output_limit(()->Base.print_matrix(io, M))
+
+
+function Base.A_mul_Bt(F1::AbstractHemiCholesky, F2::AbstractHemiCholesky)
+    L = convert(Matrix, F1)
+    if F1 === F2
+        return L*L'
     end
-    PureHemiCholesky(L)
+    L2 = convert(Matrix, F2)
+    return L1*L2'
 end
-Base.cholfact(::Type{PureHemi}, A::AbstractMatrix, δ=defaultδ(A)) = cholfact(PureHemi{floattype(eltype(A))}, A, δ)
+Base.A_mul_Bc(F1::AbstractHemiCholesky, F2::AbstractHemiCholesky) = A_mul_Bt(F1, F2)
 
-# Blocked, cache-friendly version
+function Base.cholfact{T}(::Type{PureHemi{T}}, A::AbstractMatrix, δ=defaultδ(A); blocksize=default_blocksize(T))
+    size(A, 1) == size(A, 2) || throw(DimensionMismatch("A must be square"))
+    A0 = Array(floattype(T), size(A))
+    copy!(A0, A)
+    cholfact!(PureHemi{T}, A0, δ; blocksize=blocksize)
+end
+Base.cholfact(::Type{PureHemi}, A::AbstractMatrix, δ=defaultδ(A); blocksize=default_blocksize(floattype(eltype(A)))) = cholfact(PureHemi{floattype(eltype(A))}, A, δ; blocksize=blocksize)
+
+# Blocked, cache-friendly algorithm
 function Base.cholfact!{T<:AbstractFloat}(::Type{PureHemi{T}}, A::AbstractMatrix{T}, δ=defaultδ(A); blocksize=default_blocksize(T))
     size(A,1) == size(A,2) || error("A must be square")
     eltype(A)<:Real || error("element type $(eltype(A)) not yet supported")
@@ -100,7 +112,9 @@ function Base.cholfact!{T<:AbstractFloat}(::Type{PureHemi{T}}, A::AbstractMatrix
             update_columns!(B22, d1, B21)
         end
     end
-    HemiCholeskyReal(A, d)
+    L = HemiCholeskyReal(A, d)
+    X, Y, H = solve_singularities(L, d)
+    HemiCholeskyXY(L, X, Y, H)
 end
 Base.cholfact!{T<:AbstractFloat}(::Type{PureHemi}, A::AbstractMatrix{T}, δ=defaultδ(A); blocksize=default_blocksize(T)) = cholfact!(PureHemi{T}, A, δ; blocksize=blocksize)
 
@@ -213,156 +227,131 @@ function update_columns!(dest, d::AbstractVector, C::AbstractMatrix)
     dest
 end
 
+function solve_singularities(L, d)
+    ns = sum(d .== 0)
+    K = size(L, 1)
+    T = real(eltype(L))
+    X = Array(T, K, ns)
+    H = Array(T, ns, ns)
+    Y = Array(PureHemi{T}, K, ns)
+    ns == 0 && return X, Y, H
+    forwardsubst!(Y, L)
+    backwardsubst!(X, H, L, Y)
+    X, Y, H
+end
 
-function (\){T}(F::PureHemiCholesky{T}, b::AbstractVector)
+function (\){T}(F::HemiCholeskyXY{T}, b::AbstractVector)
     L = F.L
     K = length(b)
     size(L,1) == K || throw(DimensionMismatch("rhs must have a length ($(length(b))) consistent with the size $(size(L)) of the matrix"))
-    # Determine the singular diagonals
+    ytilde = Array(PureHemi{T}, K)
+    ns = size(F.X, 2)
+    forwardsubst!(ytilde, L, b)
+    htilde = Array(T, ns)
+    xtilde = Array(T, K)
+    backwardsubst!(xtilde, htilde, L, ytilde)
+    ns == 0 && return xtilde
     indxsing = singular_diagonals(L)
-    if isempty(indxsing)
-        y = forwardsubst(L, b, indxsing, eltype(b)[])
-    else
-        # There were singular columns. We first have to determine α, the
-        # μ-components of the forward-substitution solution for y in the
-        # singular columns.
-        ytilde, Y = forwardsubst(L, b, indxsing)
-        xtilde, X, H, htildeα = backwardsubst(L, ytilde, Y, indxsing)
-        α, Δb = resolve(H, htildeα, indxsing, b)
-        y = forwardsubst(L, b+Δb, indxsing, α)
+    α, Δb = resolve(F.H, htilde, indxsing, b)
+    if any(Δb .!= 0)
+        forwardsubst!(ytilde, L, b+Δb)
+        backwardsubst!(xtilde, htilde, L, ytilde)
     end
-    backwardsubst(L, y)
+    xtilde+F.X*α
 end
 
-# Forward-substitution when there are no singular columns, or when you
-# know the value α of the μ-component in the singular columns.
-function forwardsubst{T}(L::Matrix{PureHemi{T}}, b, indxsing, α)
-    K = length(b)
-    y = fill(zero(PureHemi{T}), K)
-    si = 0
+# Forward-substitution with right hand side zero: find the
+# symmetric-division nullspace of L
+function forwardsubst!(Y, L::AbstractHemiCholesky)
+    K, ns = size(Y, 1), size(Y, 2)
+    T = real(eltype(Y))
+    fill!(Y, zero(eltype(Y)))
+    gα = Array(T, ns)   # α-coefficient on current row
+    si = 0              # number of singular columns processed so far
     for i = 1:K
-        r = b[i]
-        for j = 1:i-1
-            r -= L[i,j]*y[j]
-        end
-        Lii = L[i,i]
-        if issingular(Lii)
-            y[i] = PureHemi{T}(α[si+=1], r/Lii.m)
-        else
-            y[i] = r/Lii
-        end
-    end
-    y
-end
-
-# Forward-substitution with placeholders for the undetermined
-# μ-components α of the singular columns.
-function forwardsubst{T}(L::Matrix{PureHemi{T}}, b, indxsing=singular_diagonals(L))
-    K = length(b)
-    ns = length(indxsing)
-    ytilde = fill(zero(PureHemi{T}), K) # solution with α=0, y = ytilde + Y*α
-    Y = fill(zero(PureHemi{T}), K, ns)  # coefficients of α on each row
-    gα = Array(T, ns)                   # α-coefficient on current row
-    si = 0 # number of singular columns processed so far
-    for i = 1:K
-        g = b[i]
         for jj = 1:si
             gα[jj] = 0
         end
         for j = 1:i-1
-            Lij = L[i,j]
-            g -= Lij*ytilde[j]
+            Lij = unsafe_getindex(L, i, j)
             for jj = 1:si
                 gα[jj] -= Lij*Y[j,jj]
             end
         end
-        Lii = L[i,i]
+        Lii = unsafe_getindex(L, i, i)
         if issingular(Lii)
-            ytilde[i] = PureHemi{T}(0, g/Lii.m)
             for jj = 1:si
                 Y[i,jj] = PureHemi{T}(0, gα[jj]/Lii.m)
             end
             # Add a new singular column
             si += 1
-            Y[i,si] = PureHemi{T}(1, 0)  # add α[si]*μ to y[i]
+            Y[i,si] = PureHemi{T}(1, 0)
         else
-            ytilde[i] = g/Lii
             for jj = 1:si
                 Y[i,jj] = gα[jj]/Lii
             end
         end
     end
-    ytilde, Y
+    Y
 end
 
-# Backward-substitution
-function backwardsubst{T}(L::Matrix{PureHemi{T}}, y)
-    K = length(y)
-    x = Array(T, K)
-    for i = K:-1:1
-        h = y[i]
-        for j = i+1:K
-            Lji = L[j,i]
-            h -= Lji*x[j]
+function forwardsubst!(ytilde::AbstractVector, L::AbstractHemiCholesky, b::AbstractVector)
+    K = length(ytilde)
+    length(b) == size(L, 1) == K || throw(DimensionMismatch("Sizes $(size(y)), $(size(L)), and $(size(b)) do not match"))
+    T = real(eltype(ytilde))
+    for i = 1:K
+        g = b[i]
+        for j = 1:i-1
+            Lij = unsafe_getindex(L, i, j)
+            g -= Lij*ytilde[j]
         end
-        Lii = L[i,i]
+        Lii = unsafe_getindex(L, i, i)
         if issingular(Lii)
-            x[i] = h.m
+            ytilde[i] = PureHemi{T}(0, g/Lii.m)
         else
-            x[i] = h/Lii
+            ytilde[i] = g/Lii
         end
     end
-    x
+    ytilde
 end
 
-# Backward-substitution with placeholders for the undetermined
-# μ-components α of singular columns.
-function backwardsubst{T}(L::Matrix{PureHemi{T}}, y, Y, indxsing=singular_diagonals(L))
-    K = length(y)
-    si = length(indxsing)
-    ns = si
-    xtilde = Array(T, K)
-    X = zeros(T, K, si)
-    H = Array(T, si, si)
-    htildeα = Array(T, si)
-    hα = Array(PureHemi{T}, si)
-    ni = 0
+function backwardsubst!(X, H, L::AbstractHemiCholesky, Y)
+    K, nc = size(Y, 1), size(Y, 2)
+    size(X, 1) == K && size(X, 2) == nc || throw(DimensionMismatch("Sizes $(size(X)) and $(size(Y)) of X and Y must match"))
+    T = real(eltype(Y))
+    h = Array(PureHemi{T}, nc)  # the current row
+    si = ns = size(H, 1)        # number of singular diagonals
     for i = K:-1:1
-        htilde = y[i]
-        for jj = 1:ns
-            hα[jj] = Y[i,jj]
+        for jj = 1:nc
+            h[jj] = Y[i,jj]
         end
         for j = i+1:K
-            Lji = L[j,i]
-            htilde -= Lji*xtilde[j]
-            for jj = 1:ns
-                hα[jj] -= Lji*X[j,jj]
+            Lji = unsafe_getindex(L, j, i)
+            for jj = 1:nc
+                h[jj] -= Lji*X[j,jj]
             end
         end
-        Lii = L[i,i]
+        Lii = unsafe_getindex(L, i, i)
         if issingular(Lii)
-            xtilde[i] = htilde.m
-            htildeα[si] = -htilde.n
-            for jj = 1:ns
-                h = hα[jj]
-                H[si,jj] = h.n
-                X[i,jj] = h.m
+            for jj = 1:nc
+                hjj = h[jj]
+                X[i,jj] = hjj.m
+                H[si,jj] = hjj.n
             end
             si -= 1
         else
-            xtilde[i] = htilde/Lii
-            for jj = 1:ns
-                X[i,jj] = hα[jj]/Lii
+            for jj = 1:nc
+                X[i,jj] = h[jj].m/Lii.m
             end
         end
     end
-    xtilde, X, H, htildeα
+    X
 end
 
 function resolve(H, htildeα, indxsing, b)
-    α = svdfact(H)\htildeα
+    α = -(svdfact(H)\htildeα)
     Δb = zeros(b)  # correction to b to put it in the range of A
-    Δb[indxsing] = H*α - htildeα
+    Δb[indxsing] = H*α + htildeα
     α, Δb
 end
 
@@ -377,6 +366,7 @@ function singular_diagonals(L)
     end
     indxsing
 end
+singular_diagonals(L::HemiCholeskyReal) = find(L.d .== 0)
 
 floattype{T<:AbstractFloat}(::Type{T}) = T
 floattype{T<:Integer}(::Type{T}) = Float64
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,11 +5,11 @@ const mult_safe = VERSION >= v"0.5.0-dev"   # julia #14293
 
 A = [2 0; 0 2]
 F = cholfact(PureHemi, A)
-@test F.L == [μ+ν 0; 0 μ+ν]
+@test convert(Matrix, F) == [μ+ν 0; 0 μ+ν]
 A = [2 1; 1 2]
 F = cholfact(PureHemi, A)
-@test F.L == [μ+ν 0; (μ+ν)/2 (sqrt(3)/2)*(μ+ν)]
-@test_approx_eq F.L*F.L' A
+@test convert(Matrix, F) == [μ+ν 0; (μ+ν)/2 (sqrt(3)/2)*(μ+ν)]
+@test_approx_eq F*F' A
 b = rand(size(A,2))
 x = F\b
 @test_approx_eq x A\b
@@ -17,7 +17,7 @@ x = F\b
 A = rand(7,5); A = A'*A
 F = cholfact(PureHemi, A)
 if mult_safe
-    @test_approx_eq F.L*F.L' A
+    @test_approx_eq F*F' A
 end
 b = rand(size(A,2))
 x = F\b
@@ -26,13 +26,13 @@ x = F\b
 A = rand(3,5); A = A'*A
 F = cholfact(PureHemi, A)
 if mult_safe
-    @test_approx_eq F.L*F.L' A
+    @test_approx_eq F*F' A
 end
 
 # An indefinite matrix
 A = [-1 0; 0 1]
 F = cholfact(PureHemi, A)
-@test_approx_eq F.L*F.L' A
+@test_approx_eq F*F' A
 b = rand(size(A,2))
 x = F\b
 @test_approx_eq x A\b
@@ -40,26 +40,8 @@ x = F\b
 # A matrix that has no conventional LL' or LDL' factorization
 A = [0 1; 1 0]
 F = cholfact(PureHemi, A)
-@test_approx_eq F.L*F.L' A
+@test_approx_eq F*F' A
 b = rand(size(A,2))
-# low-level interface
-indxsing = [1,2]
-ytilde, Y = HemirealFactorizations.forwardsubst(F.L, b, indxsing)
-@test ytilde == b*ν
-@test Y == [μ 0; -ν μ]
-α = rand(2)
-yα1 = ytilde + Y*α
-yα2 = HemirealFactorizations.forwardsubst(F.L, b, indxsing, α)
-@test_approx_eq yα1 yα2
-x, X, H, htildeα = HemirealFactorizations.backwardsubst(F.L, ytilde, Y, indxsing)
-@test x == [0,0]
-@test X == eye(2)
-@test_approx_eq H -A
-@test_approx_eq htildeα -b
-α, Δb = HemirealFactorizations.resolve(H, htildeα, indxsing, b)
-@test Δb == [0,0]  # because the matrix isn't singular
-@test α == reverse(b)
-# High-level interface
 x = F\b
 @test_approx_eq x A\b
 
@@ -68,15 +50,9 @@ A = [0  1 -1;
      1  8 12;
     -1 12 20]
 F = cholfact(PureHemi, A)
-@test_approx_eq F.L*F.L' A
-@test_approx_eq F.L [μ 0 0; ν 2μ+2ν 0; -ν 3μ+3ν μ+ν]
+@test_approx_eq F*F' A
+@test_approx_eq convert(Matrix, F) [μ 0 0; ν 2μ+2ν 0; -ν 3μ+3ν μ+ν]
 b = rand(size(A,2))
-# low-level interface
-ytilde, Y = HemirealFactorizations.forwardsubst(F.L, b)
-@test_approx_eq ytilde [b[1]ν, b[2]/(2μ+2ν), (b[3]-3b[2]/2)/(μ+ν)]
-@test_approx_eq Y [μ, -1/(2μ+2ν), 5/(2μ+2ν)]
-xtilde, X, H, htildeα = HemirealFactorizations.backwardsubst(F.L, ytilde, Y)
-# high-level interface
 x = F\b
 @test_approx_eq x A\b
 
@@ -86,7 +62,7 @@ b = rand(size(A,2))
 xsvd = svdfact(A)\b
 bsvd = A*xsvd
 F = cholfact(PureHemi, A)
-@test_approx_eq F.L*F.L' A
+@test_approx_eq F*F' A
 x = F\bsvd
 @test_approx_eq_eps A*x bsvd 1e-10
 
@@ -101,13 +77,10 @@ bchol = A*x
 # In-place versions. Make these big enough to test blocked algorithm.
 A = randn(201,200); A = A'*A
 F = cholfact!(PureHemi, copy(A))
-Fh = convert(HemirealFactorizations.PureHemiCholesky, F)
-@test_approx_eq Fh.L*Fh.L' A
+@test_approx_eq F*F' A
 A[1,1] = 0
 F = cholfact!(PureHemi, copy(A))
-Fh = convert(HemirealFactorizations.PureHemiCholesky, F)
-@test_approx_eq Fh.L*Fh.L' A
+@test_approx_eq F*F' A
 A = randn(199,200); A = A'*A
 F = cholfact!(PureHemi, copy(A))
-Fh = convert(HemirealFactorizations.PureHemiCholesky, F)
-@test_approx_eq Fh.L*Fh.L' A
+@test_approx_eq F*F' A
